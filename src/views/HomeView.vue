598ec8dcbf00e18efede077562a0690ff8d5ba6b
Stephen Brooks <codedev1980@gmail.com>
2023-04-02 17:09:05 -0700

    update to pages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div class="container mt-5">
+    <div class="row mb-8">
+      <div class="col-md-6 p-3">
+        <p class="lead">Hi, My Name Is</p>
+        <h1 class="display-4">John Doe</h1>
+        <h3>And I'm A Vue & Express Developer</h3>
+        <p>
+          Here you will find more about using vue. Each page uses a router link
+          that works with vue applications, just like how React has its own way.
+        </p>
+        <a href="#" class="btn btn-primary rounded-pill">Learn More</a>
+      </div>
+      <div class="col-md-6 p-3">
+        <img alt="Vue logo" src="../assets/logo.png" />
+      </div>
+    </div>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
